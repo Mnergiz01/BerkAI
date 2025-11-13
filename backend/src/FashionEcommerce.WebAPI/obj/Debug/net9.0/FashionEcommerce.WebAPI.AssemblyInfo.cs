8a5f4a49ad255e8369d7275a28e3daadf2320917
bberkaysari <bberkaysari0@gmail.com>
2025-11-13 20:29:30 +0300

    son güncellemeler

--- backend/src/FashionEcommerce.WebAPI/obj/Debug/net9.0/FashionEcommerce.WebAPI.AssemblyInfo.cs
+++ backend/src/FashionEcommerce.WebAPI/obj/Debug/net9.0/FashionEcommerce.WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FashionEcommerce.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d0d02d7d4c31e336a0c6bef112c3d09639f7555")]
 [assembly: System.Reflection.AssemblyProductAttribute("FashionEcommerce.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FashionEcommerce.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
